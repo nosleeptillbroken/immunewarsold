cd4b1a3808fb4a41d749256263e8732c08078c04
J T <fff4word@live.ca>
2016-01-17 17:18:52 -0500

    Baked  NavMesh onto the Non-Tower Spawning plane. Created an Invisible wall to avoid on the Navmesh. Made invisibles for TargetNode to interact with NavAgent Script, and creep spawning. Started working on spawning/de-spawning code for creeps, but didn't finish it yet. Placed SampleCreep and Node for demo.

---
+++ Assets/Scripts/CreepSpawn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreepSpawn : MonoBehaviour {
+	public GameObject creep;
+	public Vector3 spawnValues;
+
+	public int creepCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	void Start ()
+	{
+		StartCoroutine (SpawnWaves ());
+	}
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0;i < creepCount; i++) 
+			{
+				Vector3 spawnPosition = new Vector3 (spawnValues.x, spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (creep, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+}
