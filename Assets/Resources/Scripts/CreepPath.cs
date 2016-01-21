a0940dcbc1952d385134d6cef080ed6dc8e4c3c4
Dan Yeomans <danyeomans98@gmail.com>
2016-01-20 22:26:44 -0500

    Add path editor capabilities Add basic class for creep path

---
+++ Assets/Resources/Scripts/CreepPath.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Specifies a path (or paths) that creeps can follow.
+/// </summary>
+public class CreepPath : MonoBehaviour {
+
+    public List<Vector3> points = new List<Vector3>();
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < points.Count-1; i++)
+        {
+            Debug.DrawLine(points[i], points[i + i], Color.blue);
+        }
+
+    }
+
+}
---
+++ Assets/Resources/Scripts/CreepPathEditor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using UnityEditor;
+
+[@CustomEditor(typeof(CreepPath))]
+public class CreepPathEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+
+        CreepPath path = (CreepPath)target;
+        List<Vector3> points = path.points;
+        
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Add Point")) {
+
+            points.Add(Vector3.zero);
+
+        }
+    }
+
+    void OnSceneGUI()
+    {
+        Handles.color = Color.blue;
+
+        CreepPath path = (CreepPath)target;
+        List<Vector3> points = path.points;
+
+        Handles.Label(path.transform.position, "Origin");
+        
+        for (int i = 0; i < points.Count;i++)
+        {
+            Handles.lighting = false;
+            points[i] = Handles.PositionHandle(points.ElementAt(i), Quaternion.identity);
+            Handles.lighting = true;
+            
+            Handles.Label(points[i], i.ToString());
+            Handles.DrawSolidDisc(points[i], Vector3.up, 0.05f);
+
+            if (i+1 < points.Count)
+            {
+                Handles.DrawLine(points[i], points[i+1]);
+            }
+        }
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+
+    } 
+
+}
