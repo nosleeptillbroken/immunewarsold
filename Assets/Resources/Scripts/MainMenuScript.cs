91778e7dd086a76c81565b2d71d0fc3a26b56550
Mike Shannon <mike.shannon01@gmail.com>
2016-01-22 10:30:30 -0500

    Main menu with Start and Exit functionality. Player Health bar in basic working shape.

---
+++ Assets/Resources/Scripts/MainMenuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour 
+{
+
+	public Canvas quitMenu;
+	public Button startButton;
+	public Button exitButton;
+
+	void Start()
+	{
+		quitMenu = quitMenu.GetComponent<Canvas>();
+		startButton = startButton.GetComponent<Button>();
+		exitButton = exitButton.GetComponent<Button>();
+		quitMenu.enabled = false;
+	}
+
+	public void exitPressed()
+	{
+		quitMenu.enabled = true;
+		startButton.enabled = false;
+		exitButton.enabled = false;
+	}
+
+	public void noPressed()
+	{
+		quitMenu.enabled = false;
+		startButton.enabled = true;
+		exitButton.enabled = true;
+	}
+
+	public void startLevel()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void exitGame()
+	{
+		Application.Quit();
+	}
+}
---
+++ Assets/Resources/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour 
+{
+	public Slider healthBar;	// sets up healthBar to be associated with UI Slider Graphic 
+
+	void Start () 
+	{
+		healthBar.value = 100;	// initializes player health at 100%
+	}
+		
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.C))	//	Temporary function to test health decrease
+		{
+			healthBar.value -=5;		// If the C button is pressed, health decreases by 5pts
+		}	
+	}
+}
