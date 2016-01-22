58089765a1900ad098c5a82f5bb68b3435689386
Andrew MacMillan <andrewmacmilan1@hotmail.com>
2016-01-22 12:45:35 -0500

    tower will shoot at enemy. spawns bullet. placeholder enemy

---
+++ Assets/Resources/Scripts/bullet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+	public bool hit = false;
+	public float min=1f;
+	public float max=12f;
+	public Tower _TurretScript;
+	public Vector3 bulletman;
+
+	// Use this for initialization
+	void Start () {
+		min=transform.position.x;
+		max=transform.position.x+6;
+		//bulletman = _TurretScript.selectedTarget.transform.position;
+	}
+
+
+
+	// Update is called once per frame
+	 void Update () 
+	{
+		
+		//transform.Translate(Vector3.forward * 10.0f * Time.deltaTime);
+
+	}
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.CompareTag("Enemy")){
+			// call the function TakeDamage(10) in the hit object, if any
+			//i need to call the specific bullet, otherwise all enemies take damage. take not for future sessions
+			//transform.position = Vector3.Lerp (transform.position, bulletman, _TurretScript.speed * Time.deltaTime);
+			col.gameObject.SendMessage("TakeDamage", 10, SendMessageOptions.DontRequireReceiver);
+		}
+		Destroy(gameObject); // bullet suicides after hitting anything
+	}
+}
---
+++ Assets/Resources/Scripts/enemy_movement_sample.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy_movement_sample : MonoBehaviour {
+	public GameObject Bullet;
+
+	public float min=5f;
+	public float max=6f;
+	public float Health = 50;
+	public Tower _TurretScript;
+	// Use this for initialization
+	void Start () {
+		//Debug.Log ("HIIIIIIII",gameObject);
+		min=transform.position.x;
+		max=transform.position.x+6;
+		//
+		//
+	}
+
+	void TakeDamage(int damage){
+		Health -= damage;
+		if (Health <= 0) {
+			//_TurretScript.SortTargetsByDistance ();
+			Destroy(gameObject);
+			//_TurretScript.targets.Remove(GameObject.FindGameObjectWithTag("Enemy").transform);
+			//_TurretScript.selectedTarget = _TurretScript.targets [0];
+
+
+			//gameObject.GetComponent<TurretScript> ().SortTargetsByDistance ();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+			transform.position = new Vector3 (Mathf.PingPong (Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
+
+	}
+
+}
